82b6b51a6d89bb9ec39cca1c4f76935fe301d95c
AadOfzo <aadofzo@gmail.com>
2023-04-28 16:21:47 +0200

    28-04-2023 UD-2: Code working!!! SongServiceTest added. UD-1: Code working!!! Songs can be added with Post in Postman. J-unit test for SongController testing 6/6.
    -----------------------------------------------
    26-04-2023
    UD-4: final commit, code working.
          SongCollectionRepository made
    
    Ud-4: song_collection_types defined in data.sql.
    
    UD-3: data.sql working song_collection_type showing in Postgres.
    Code working!
    
    UD-2: Code Running again data.sql not working: table users cannot be resolved
    
    UD-1: Last commit before delete song modules
    
    -------------------------------------------------
    25-04-2023
    UD-2:
    - MusicCollection, MusicController created for Playlists.
    - Collections don't have CRUD functionality
    
    UD-1:
    Added SongCollectionTypes to SongService
    ------------------------------------------------
    24-04-2023
    
    UD-1: Song Modules working code running!!
    - Songs are created in PostgreSQL, but no collections added.
    - Users cannot post in Postman (also songs)
    
    ------------------------------------------------
    21-04-2023
    UD-3: Jwt Encryption added.
          Code running!
    
    UD-2: Code Not Running
    
    UD-1: Code running!
    requestMatchers for "/songs" & "/songCollectionTypes"
    added to SpringSecurityConfig
    
    -----------------------------------------------
    20-4-2023
    
    UD-3: Backup point before adding SongCollectionType modules: Demo, Sample Demo, Single, EP, Album
    
    UD-2: Song Modules working App starting
    Song / SongController / SongDto / SongInputDto / SongRepository / SongService / SongTitleNotFoundException.
    
    No errors!!
    
    UD-1: Start song modules
    
    ---------------------------------------------
    19-4-2023:
    First working commit from TechItEasy

---
+++ src/test/java/Journey_of_Taro_V3/Journey_of_Taro_V3/services/SongServiceTest.java
@@ -0,0 +1,84 @@
+package Journey_of_Taro_V3.Journey_of_Taro_V3.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Journey_of_Taro_V3.Journey_of_Taro_V3.dtos.SongDto;
+import Journey_of_Taro_V3.Journey_of_Taro_V3.exceptions.SongTitleNotFoundException;
+import Journey_of_Taro_V3.Journey_of_Taro_V3.models.Song;
+import Journey_of_Taro_V3.Journey_of_Taro_V3.repositories.SongRepository;
+
+public class SongServiceTest {
+
+    private SongRepository songRepository;
+    private SongService songService;
+
+    @Before
+    public void setUp() {
+        songRepository = mock(SongRepository.class);
+        songService = new SongService(songRepository);
+    }
+
+    @Test
+    public void testGetAllSongs() {
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song(1L, "song1", "artist1", true));
+        songs.add(new Song(2L, "song2", "artist2", false));
+
+        when(songRepository.findAll()).thenReturn(songs);
+
+        List<SongDto> songDtos = songService.getAllSongs();
+
+        assertEquals(2, songDtos.size());
+        assertEquals("song1", songDtos.get(0).getSongtitle());
+        assertEquals("artist2", songDtos.get(1).getArtistname());
+    }
+
+    @Test
+    public void testGetAllSongsBySongTitle() {
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song(1L, "song1", "artist1", true));
+        songs.add(new Song(2L, "song2", "artist2", false));
+
+        when(songRepository.findAllSongsBySongtitleEqualsIgnoreCase("song1")).thenReturn(songs.subList(0, 1));
+
+        List<SongDto> songDtos = songService.getAllSongsBySongTitle("song1");
+
+        assertEquals(1, songDtos.size());
+        assertEquals("song1", songDtos.get(0).getSongtitle());
+        assertEquals("artist1", songDtos.get(0).getArtistname());
+        assertEquals(true, songDtos.get(0).getIsfavorited());
+    }
+
+    @Test(expected = SongTitleNotFoundException.class)
+    public void testGetSongByIdWithException() {
+        when(songRepository.findById(1L)).thenReturn(Optional.empty());
+
+        songService.getSongById(1L);
+    }
+
+
+    @Test
+    public void testGetSongById() {
+        Song song = new Song(1L, "song1", "artist1", true);
+
+        when(songRepository.findById(1L)).thenReturn(java.util.Optional.of(song));
+
+        SongDto songDto = songService.getSongById(1L);
+
+        assertEquals("song1", songDto.getSongtitle());
+        assertEquals("artist1", songDto.getArtistname());
+        assertEquals(true, songDto.getIsfavorited());
+    }
+
+    // addSong, updateSong, deleteSong test cases can also be added similarly
+
+}
