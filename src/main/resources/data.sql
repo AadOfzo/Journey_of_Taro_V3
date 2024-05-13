6c62f1d6ca1e187cab74120a8c936cca02aab32e
AadOfzo <aadofzo@gmail.com>
2024-05-13 13:20:03 +0200

    Television classes verwijderd.
    Mee bezig:
    data.sql nieuwe Admin en User aan maken met encryptie.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@
 -- insert into users(id, username, password, apikey, firstname, lastname, email)
 -- VALUES (1001, 'Example_User', 'ExamplePassword1', 713468367, 'TestFirstName', 'TestLastName', 'example_user@email.com');
 insert into users(id, username, password, apikey, firstname, lastname, dob, country, email, artistname)
-VALUES (1001, 'Example_Admin_1', 'ExamplePassword1', 713468367, 'Test_FirstName_1', 'Test_LastName_1', '18-03-2023', 'the Netherlands','admin@testemail.com', 'Test_Artist_Name_1');
---        (1002, 'Example_User_1', 'ExamplePassword1', 713468368, 'Test_FirstName_1', 'Test_LastName_1', '19-04-2024', 'the Netherlands','user@testemail.com', 'Test_Artist_Name_1', 'USER');
+VALUES (1001, 'Example_Admin_1', 'ExamplePassword1', 713468367, 'Test_FirstName_1', 'Test_LastName_1', '18-03-2023', 'the Netherlands','admin@testemail.com', 'Test_Artist_Name_1'),
+       (1002, 'Example_User_1', 'ExamplePassword1', 713468368, 'Test_FirstName_1', 'Test_LastName_1', '19-04-2024', 'the Netherlands','user@testemail.com', 'Test_Artist_Name_1');
 
 -- User Authorities
 insert into authorities(user_id, authority)
