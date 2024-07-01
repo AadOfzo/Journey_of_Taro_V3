78a4d513b25093f8a39cd6bbc7070a393a477721
AadOfzo <aadofzo@gmail.com>
2024-07-01 16:43:23 +0200

    TErug van naar main branch van datasql2 van 20 Juni

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@
 -- insert into users(id, username, password, apikey, firstname, lastname, email)
 -- VALUES (1001, 'Example_User', 'ExamplePassword1', 713468367, 'TestFirstName', 'TestLastName', 'example_user@email.com');
 insert into users(id, username, password, apikey, firstname, lastname, dob, country, email, artistname)
-VALUES (1001, 'Example_Admin_1', '$2a$10$wtKjdXOpg9dP..1xbxk2ouv6rWOW.kGaX1vhKxgAy.G6aGL./AGVW', '7lcjpziAtuwtTyjaIUeq', 'Test_FirstName_1', 'Test_LastName_1', '18-03-2023', 'the Netherlands','admin@testemail.com', 'Test_Artist_Name_1'),
+VALUES (1001, 'Example_Admin_1', '$2a$10$wtKjdXOpg9dP..1xbxk2ouv6rWOW.kGaX1vhKxgAy.G6aGL./AGVW', '7lcjpziAtuwtTyjaIUeq', 'Test_FirstName_1', 'Test_LastName_1', '18-03-2023', 'the Netherlands','admin@testemail.com', 'Admin_Artist_Name_1'),
        (1002, 'Example_User_1', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJFeGFtcGxlX1VzZXJfMSIsImlhdCI6MTcxODg4MTMwNCwiZXhwIjoxNzE5NzQ1MzA0fQ.0mGAxJNNSp4_T4I9FBBQYMAEgn_PO8sbntN9jTzS8FU', 'sQXMUVcOjXe83PeUJAfN', 'Test_FirstName_1', 'Test_LastName_1', '21-05-2013', 'Belgium','user@testemail.com', 'User_Artist');
 --        (1003, 'Example_User_1', 'ExamplePassword1', 713468369, 'Test_FirstName_1', 'Test_LastName_1', '19-04-2024', 'the Netherlands','user@testemail.com', 'Test_Artist_Name_1')
 
