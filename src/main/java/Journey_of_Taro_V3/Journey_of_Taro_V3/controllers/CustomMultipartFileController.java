814f0c2472efacc9a73ee08be5bef3b4a8f5b11b
AadOfzo <aadofzo@gmail.com>
2024-07-12 15:14:44 +0200

    Reset naar 10/7 /fileUpload werkt weer

--- src/main/java/Journey_of_Taro_V3/Journey_of_Taro_V3/controllers/CustomMultipartFileController.java
+++ src/main/java/Journey_of_Taro_V3/Journey_of_Taro_V3/controllers/CustomMultipartFileController.java
@@ -15,7 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
